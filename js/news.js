e0f6aa51d65bec25b7564a037fa45bce6bfb09a5
Nolan Lynch <75101303+mush298@users.noreply.github.com>
2024-07-10 21:45:09 -0500

    Create news.js

---
+++ js/news.js
@@ -0,0 +1,182 @@
+let messages = [`It was a normal spring day in The Loud House. The 10 sisters, Lori, Leni, Luna, Luan, Lynn Jr, Lucy, Lana, Lola, Lisa, and Lily Loud, were all in an argument. The only son and brother, Lincoln Loud, was not in the argument, however. He was up in his room, reading an Ace Savvy comic.
+
+Lincoln: (to reader) Oh, hi. You probably hear the arguing going on downstairs. Yep, my 10 sisters are always arguing. You're probably wondering why I'm not in the argument. Well, lately, I've been staying out of the arguments, mostly because I'm not too worried about them anymore. I'm more worried about Dad. He's changed, well, a LOT ever since he started drinking. That's honestly the main reason I'm not looking forward to summer vacation.
+
+Their father, Lynn Sr., had begun drinking because he felt it was the only way to handle his kids. Every day, he felt his sanity weaken. Rita, the kids mother, did what she could to calm him down. But today, not even she would be able to stop him.
+
+Lincoln went back to his comic. Outside the kids bedrooms, Rita and Lynn Sr were arguing. And Lynn Sr. looked really angry.
+
+Rita: They're our children, dear! You can't hit them for arguing, that's considered child abuse!
+
+Lynn Sr.: Oh, shut up, Rita! Only one of them has learned, and that's not enough to calm me down!
+
+Rita: Look, they're kids! Of course they're going to make mistakes!
+
+Lynn Sr.: (very angry) MISTAKES?!!? HOW CAN 10 GIRLS MAKE THIS MANY MISTAKES?!!?
+
+Lynn Sr.'s tone echoed throughout the entire house, causing the sisters to stop arguing and Lincoln to look up from his comic.
+
+Lincoln: (to reader) Uh oh, when Dad's voice is that loud, NO ONE is safe.
+
+Lincoln gets off his bed and props his door open to see his father going down the stairs. He then opens his door all the way to see his mother looking worried.
+
+Lincoln: Jeez Mom, what's with him?
+
+Rita: He just needs to blow off some steam, that's all.
+
+Suddenly, there was a loud slapping sound that came from downstairs. Lincoln and Rita's eyes widened.
+
+Both: NO HE DIDN'T!
+
+They both ran down the stairs to see Lori on her knees, holding her cheek with tears running down her face.
+
+Lynn Sr.: (to Lori) YOU'RE SUPPOSED TO BE A ROLE MODEL FOR YOUR SISTERS, BUT YOU CAN'T EVEN DO THAT!!! (to all) ALL OF YOU, YOU'RE JUST PATHETIC EXCUSES FOR DAUGHTERS!!!
+
+Rita: (very angry) LYNN LOUD SR, WHAT IS WRONG WITH YOU?!!? HOW COULD YOU HIT ONE OF OUR DAUGHTERS?!!?
+
+Lynn Sr.: JUST WATCH ME HIT THE REST AND YOU'LL SEE!!!
+
+Leni got down to protect and comfort Lori, while the other older sisters got in front of the younger sisters. Lynn Sr. raised his hand to slap Leni.
+
+Lincoln: Dad, THAT'S ENOUGH!
+
+Lincoln ran to stop his father by grabbing him on the arm.
+
+Lynn Sr.: (looking back to Lincoln) LET GO, YOU PATHETIC WHITE HAIRED EXCUSE FOR A SON!
+
+Lynn Sr. thrashed Lincoln off of his arm. He then grabbed Lincoln and threw him over his shoulder onto the coffee table, causing it to smash underneath him. The glass on the table smashed as well, causing shards to puncture Lincoln's arms, legs, and back.
+
+Rita and Sisters: (in horror) LINCOLN!!!
+
+Rita and the sisters ran over to Lincoln. Leni lifts up Lincoln, while trying not to move the glass shards. As Lynn Sr. stood over his son's bloodied body, he began to realize what he had done.
+
+Luna: (angrily crying) YOU HEARTLESS MONSTER!!! HOW COULD YOU DO SOMETHING LIKE THIS?!!?
+
+Lola: (angrily crying) NOW YOU'VE GONE TOO FAR!
+
+Lynn Sr.: I-I'm sorry. (walks towards Lincoln) H-Here, let me help.
+
+Rita stands up, walks to her husband, and slaps him across the face. The sisters looked in shock, because Rita had now reached a level of anger that not even LOLA reaches.
+
+Rita: (angrily) YOU STAY AWAY FROM LINCOLN, YOU PATHETIC EXCUSE FOR A FATHER AND HUSBAND!!! GET OUT OF THIS HOUSE, RIGHT NOW!!!! I DON'T EVER WANT TO SEE YOUR DISGUSTING FACE IN THIS NEIGHBORHOOD EVER AGAIN!!!!
+
+Lynn Jr gets up and pushes Lynn Sr. out the door. He tries to fight back, only for Jr to square him right in the jaw.
+
+Lynn Sr.: Jr...
+
+Lynn: (angrily crying) GO GET HIT BY A CAR, YOU SICK FREAK!!!
+
+Lynn slams the door in his face, then she and Rita ran back to Lincoln.
+
+Leni: Please Linky, stay with us!
+
+Back outside, Lynn Sr. was being approached by Lori's boyfriend, Bobby Santiago, and his younger sister, Lincoln's girlfriend, Ronnie Anne.
+
+Bobby: Mr. Loud? What's going on?
+
+Lynn Sr.: My family kicked me out because of what I did.
+
+Ronnie Anne: What DID you do?
+
+Lynn Sr.: I... threw Lincoln onto the coffee table... and injured him badly.
+
+Ronnie Anne: (angrily surprised) YOU WHAT?!!?
+
+Ronnie Anne kicks Lynn Sr. in the leg, and almost breaks it. She then runs to the Loud House with Bobby behind her.
+
+Back inside the Loud House, the girls are trying to bandage Lincoln to stop the bleeding, but they can't take the glass shards out because they are afraid it will make him bleed out more. Suddenly there was a loud banging at the door. Lori gets up to open the door. She's surprised to see Bobby and Ronnie Anne, both looking worried.
+
+Lori: Boo Bear? Ronnie Anne?
+
+Ronnie Anne: (panicked) WHERE'S LINCOLN?!!?
+
+Lori: In there.
+
+Ronnie Anne runs into the house, but stops when she sees Lincoln. She covers her mouth in shock and begins tearing up.
+
+Ronnie Anne: (voice breaking) OH GOD!
+
+Luan: (sees Ronnie Anne) Guys, make room for Ronnie Anne.
+
+The girls back away from Lincoln enough to let Ronnie Anne kneel beside Lincoln's head.
+
+Bobby: What exactly happened?!
+
+Lori: That scumbag of a father threw him onto the coffee table when he tried to save Leni from getting slapped. He already got me.
+
+Bobby: Jeez, that explains why your cheek seems more red.
+
+Lucy: Then Mom slapped him across the face and Lynn punched him out the door.
+
+Ronnie Anne: And, I may have gotten my own pain on him by kicking him in the shin.
+
+Lana: (looking worried) Guys, Lincoln doesn't look like he's breathing!
+
+Rita: (checking his pulse, then panicking) He's barely breathing!
+
+Luna: Those medical dudes need to hurry up!
+
+Then a medical officer barges in the door, having been left open by Bobby.
+
+Medical Officer: Back away, we'll handle this!
+
+Everyone backs away from Lincoln as more medical officers come into the house. They load Lincoln onto a stretcher and place an oxygen mask over his nose and mouth. They then push Lincoln out of the house into an ambulance.
+
+Medical Officer: We'll allow two people to ride with him. The rest will have to follow in another vehicle.
+
+Bobby: Sis, go with him.
+
+Ronnie Anne: (voice breaking) I was going to anyway.
+
+Rita: (reaches for her keys) Here Lori, follow behind with everyone else.
+
+Lori: Okay. (to everyone else) Into Vanzilla, NOW!
+
+Rita and Ronnie Anne hop into the ambulance with Lincoln, while everyone else gets into the family van, Vanzilla. Then, they both drive off. In Vanzilla, Lana and Lola break down crying, so Leni and Luna hug them, though they were on the verge of crying, too.
+
+Leni: It's gonna be okay. Linky will be fine.
+
+Lola: (crying into Leni's dress) I hope you're right!
+
+Lana: (crying into Luna's shirt) Why did it have to be Lincoln?!!?
+
+Luna: (beginning to shed tears) I don't know, little dude. I don't know.
+
+Back in the ambulance, Ronnie Anne was holding Lincoln's hand while tears ran down her face.
+
+Ronnie Anne's Mind: Please, Lincoln! Don't die on me!`, 'When I was your age, 100 was considered a big number, well now its ' + format(layers[0].power) + ', Kids these days...', "Did you just disrespect Skibidi Toilet?  Well yes, but you dont understand... Why, why would you do this? He just has L Rizz, He's a sigma, you don't think he's a sigma. Mike- You stopped watching YouTube Shorts, and now you have L Rizz, you aren't even a sigma! I'm an even bigger sigma. Skibidi Toilet is the alpha, he is more sigma than anyone. I'm bigger than me. Cap, skibidi toilet MOGS you. No he dosen't, Aiden Ross and Kai Cenat both ratioed him- Never rizz me ever again.", "Nolan's Button Simulator: Rewritten (wip title) coming soon!"];
+
+setInterval(function() {
+    messages[1] = 'When I was your age, 100 was considered a big number, well now its ' + format(layers[0].power.pow(69420)) + ', Kids these days...';
+}, 50)
+
+
+let currentIndex = 1;
+
+function updateTicker() {
+    const ticker = document.getElementById('ticker');
+    const message = messages[currentIndex];
+    ticker.textContent = message;
+
+    // Calculate duration based on message length (adjust multiplier for speed)
+    let duration = message.length * 0.05;
+    if (duration < 5) {
+      duration = 5;
+    }// 0.15 seconds per character
+
+    // Set the animation duration
+    ticker.style.animationDuration = `${duration}s`;
+
+    // Reset the animation
+    ticker.style.animation = 'none';
+    ticker.offsetHeight; // Trigger reflow
+    ticker.style.animation = `ticker ${duration}s linear`;
+
+    // Move to next message
+    currentIndex = Math.floor(Math.random() * messages.length);
+
+    // Schedule next update
+    setTimeout(updateTicker, duration * 1000);
+}
+
+updateTicker(); // Initial update
