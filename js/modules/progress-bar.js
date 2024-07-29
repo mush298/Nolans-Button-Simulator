85cd3cda8dac1ae8c4f0c88accb2009577b4198b
Nolan Lynch <75101303+mush298@users.noreply.github.com>
2024-07-29 18:23:20 -0500

    Update progress-bar.js

--- js/modules/progress-bar.js
+++ js/modules/progress-bar.js
@@ -28,7 +28,21 @@ let quests = [{
         return player.currencies[0].value.log10().div(933).mul(100)
     },
     name: "Reach rank 12 to upgrade your tier!"
-},]
+},
+
+{
+    get percent() {
+        return player.currencies[0].value.log10().log10().div(E(9e15).log10()).mul(100)
+    },
+    name: "Reach rank 12 to upgrade your tier!"
+}, 
+{
+    get percent() {
+        return player.infinity.points.div(1e8)
+    },
+    name: "Reach rank 12 to upgrade your tier!"
+}, 
+]
 const progressBar = document.getElementById('progressBar');
 const progressText = document.getElementById('progressText');
 
@@ -44,7 +58,13 @@ function setProgress(value, content) {
     }
 }
 function getQuest() {
-    if (player.tier > 0) {
+    if (player.infinity.times.gte(1)) {
+        goal = 6
+    }
+    else if (player.tier > 1) {
+        goal = 5
+    }
+    else if (player.tier > 0) {
         goal = 4
     }
 else if (player.rank > 2) {
@@ -60,7 +80,9 @@ goal = 1
 setInterval(function() {
     quests[1].name = "Reach " + format(5e8) + " cash to upgrade your rank!"
     quests[2].name = "Reach " + format(2.5e21) + " cash to upgrade your rank!"
-    quests[4].name = "Reach " + format("1e933") + " cash to beat the game! (for now....)"
+    quests[4].name = "Reach " + format("1e933") + " cash to begin inflation I guess.."
+    quests[5].name = "Reach " + format("e9e15") + " cash to unlock a new layer"
+      quests[6].name = "Reach " + format("e8") + " infinity points to beat the game! for now..."
     getQuest()
     document.getElementsByClassName('progress-container').width = screen.width
     setProgress(quests[goal].percent, quests[goal].name)
