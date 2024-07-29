697ffceaa0dbf1ae3724affe2145fc1e73a27982
Nolan Lynch <75101303+mush298@users.noreply.github.com>
2024-07-29 18:20:14 -0500

    Update element.js

--- js/element.js
+++ js/element.js
@@ -52,17 +52,31 @@ function updateGUI() {
        } 
        for (let i = 0; i < buttons[0].length; i++) {
         buttons[0][i].innerHTML = "Buy <text-style text=\"" + "multiplier" + "\">x" + format(basegains[0].pow(i+1).mul(player.currencies[1].total)) + "</text-style> multiplier " + "for " + "<text-style text=\"" + "cash" + "\">" + format(basecosts[0].pow(i+1)) + "</text-style> cash"; 
-
+if (player.currencies[0].value.gte(basecosts[0].pow(i+1))) {
+    buttons[0][i].id = "multiplier-button"
+} else {
+    buttons[0][i].id = "multiplier-button-cant"  
+}
        }
        for (let i = 0; i < buttons[1].length; i++) {
         buttons[1][i].innerHTML = "Buy <text-style text=\"" + "rebirth" + "\">" + format(basegains[1].pow(i+1).mul(player.currencies[2].total)) + "</text-style> rebirths " + "for" + "<text-style text=\"" + "multiplier" + "\">x" + format(basecosts[1].pow(i+1)) + "</text-style>  multiplier"; 
-
+        if (player.currencies[1].value.gte(basecosts[1].pow(i+1))) {
+            buttons[1][i].id = "rebirth-button"
+        } else {
+            buttons[1][i].id = "rebirth-button-cant"  
+        }
        }
        for (let i = 0; i < buttons[2].length; i++) {
         buttons[2][i].innerHTML = "Buy <text-style text=\"" + "ultrarebirth" + "\">" + format(basegains[2].pow(i+1).mul(player.currencies[3].total)) + "</text-style> ultra rebirths " + "for" + "<text-style text=\"" + "rebirth" + "\">x" + format(basecosts[2].pow(i+1)) + "</text-style>  rebirths"; 
-
+        if (player.currencies[2].value.gte(basecosts[2].pow(i+1))) {
+            buttons[2][i].id = "ultrarebirth-button"
+        } else {
+            buttons[2][i].id = "ultrarebirth-button-cant"  
+        }
        }
+
        updateRankHTML()
+       updateInfinityHTML()
 }
 createGUI()
 setInterval(updateGUI, 20)
